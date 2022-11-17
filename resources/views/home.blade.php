0536ecbe396eb29342317ac142c9923683d835da
jihannaf13 <118423791+jihannaf13@users.noreply.github.com>
2022-11-17 13:20:14 +0700

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Home Page</title>
+  <link rel="stylesheet" href="css/swiper-bundle.min.css">
+  <link rel="stylesheet" href="css/style2.css">
+  <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+</head>
+<body>
+
+  <header>
+    <div class="nav-bar">
+      <a href="" class="logo">NomNomCat</a>
+      <!-- <img src="https://cdn.discordapp.com/attachments/984015287923929122/1042493106324512979/N_O_M_N_O_M__1_-removebg-preview.png" alt="" -->
+      <!-- width="80" 
+      height="80"> -->
+      <div class="navigation">
+        <div class="nav-items">
+          <i class="uil uil-times nav-close-btn"></i>
+          <!-- <a href="#"><i class="uil uil-home"></i> Home</a> -->
+          <a href="#"><i class="uil uil-compass"></i> <b>Notification</b></a>
+          <!-- <a href="#"><i class="uil uil-info-circle"></i> Your Cat's Data</a> -->
+          <!-- <a href="#"><i class="uil uil-document-layout-left"></i> Blog</a> -->
+          <a href="#"><i class="uil uil-envelope"></i> <b>Contact Us</b></a>
+        </div>
+      </div>
+      <i class="uil uil-apps nav-menu-btn"></i>
+    </div>
+  </header>
+
+  <section class="home">
+    <!-- <div class="media-icons">
+      <a href="#"><i class="uil uil-facebook-f"></i></a>
+      <a href="#"><i class="uil uil-whatsapp"></i></a>
+      <a href="#"><i class="uil uil-fast-mail"></i></a>
+    </div> -->
+
+    <div class="swiper bg-slider">
+      <div class="swiper-wrapper">
+        <div class="swiper-slide">
+          <img src="https://cdn.discordapp.com/attachments/984015287923929122/1042497306852859904/NomNomCat.png" alt="">
+          <div class="text-content">
+            <h2 class="title">NomNomCat!</h2>
+            <p>We will help you to taking care of your cat</p>
+            
+            <!-- <button class="read-btn">Click Here<i class="uil uil-arrow-right"></i></button> -->
+          </div>
+        </div>
+        <div class="swiper-slide dark-layer">
+          <img src="https://cdn.discordapp.com/attachments/984015287923929122/1042495335941029908/NomNomCat_1.png" alt="">
+          <div class="text-content">
+            <h2 class="title">Your Cat's Data <span>Daily Routine</span></h2>
+            <p>Your daily cat history</p>
+            <button class="read-btn">Check Data<i class=""></i></button>
+          </div>
+        </div>
+        <div class="swiper-slide dark-layer">
+          <img src="https://cdn.discordapp.com/attachments/984015287923929122/1042510099488440432/NomNomCat_3.png" alt="">
+          <div class="text-content">
+            <h2 class="title">Profile Settings</h2>
+            <!-- <p>Summer is the hottest of the four temperate seasons, occurring after spring and
+            before autumn. At or aroundthe summer solstice, the earliest sunrise and latest
+            sunset occurs, daylight hours are longest and dark hours are shortest, with day
+            length decreasing as the season progresses after the solstice.</p> -->
+            <button class="read-btn">Log Out<i class=""></i></button>
+          </div>
+        </div>
+        <div class="swiper-slide">
+          <img src="images/bg4.jpg" alt="">
+          <div class="text-content">
+            <h2 class="title">Spring <span>Season</span></h2>
+            <p>Spring, also known as springtime, is one of the four temperate seasons, succeeding
+            winter and preceding summer. There are various technical definitions of spring, but
+            local usage of the term varies according to local climate, cultures and customs. When
+            it is spring in the Northern Hemisphere, it is autumn in the Southern Hemisphere and
+            vice versa.</p>
+            <button class="read-btn">Read More <i class="uil uil-arrow-right"></i></button>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    <div class="bg-slider-thumbs">
+      <div class="swiper-wrapper thumbs-container">
+        <img src="https://cdn.discordapp.com/attachments/984015287923929122/1042509019274805298/Untitled_design_4.png" class="swiper-slide" alt="">
+        <img src="https://cdn.discordapp.com/attachments/984015287923929122/1042512628590841976/Untitled_design_5.png" class="swiper-slide" alt="">
+        <img src="https://cdn.discordapp.com/attachments/984015287923929122/1042505969114026044/Untitled_design_3.png" class="swiper-slide" alt="">
+        <!-- <img src="images/bg4.jpg" class="swiper-slide" alt=""> -->
+      </div>
+    </div>
+  </section>
+
+  <!-- <section class="about section">
+    <h2>Seasons of the year</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+    aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
+    aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+    occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+    aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
+    aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+    occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+    aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
+    aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+    occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+    aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
+    aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+    occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+    aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
+    aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+    occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+    aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
+    aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+    occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+    aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
+    aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+    occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+    aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
+    aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+    occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+    aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
+    aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+    occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+    aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
+    aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+    occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+    aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
+    aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+    occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+    aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
+    aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+    occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  </section> -->
+
+  <script src="js/swiper-bundle.min.js"></script>
+  <script src="js/main.js"></script>
+
+</body>
+</html>
+      
